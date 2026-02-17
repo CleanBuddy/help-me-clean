e1e0e26a2e632036c8c5ecb15861540d9ff15eb3
Victor Mihaita <victormihaita.iec@gmail.com>
2026-02-17 22:17:34 +0200

    cleanup

--- web/packages/client-web/src/vite-env.d.ts
+++ web/packages/client-web/src/vite-env.d.ts
@@ -2,7 +2,6 @@
 
 interface ImportMetaEnv {
   readonly VITE_GRAPHQL_ENDPOINT: string;
-  readonly VITE_WS_ENDPOINT: string;
   readonly VITE_GOOGLE_CLIENT_ID: string;
   readonly VITE_COMPANY_DASHBOARD_URL: string;
   readonly VITE_GOOGLE_MAPS_API_KEY: string;
